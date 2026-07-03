e6ae05da995b28828ef744a8b799661716c80e34
rafisgithub <rafi.cse.ahmed@gmail.com>
2026-07-03 20:54:07 +0600

    Refactor fork system call example to improve output clarity and organization

--- semister-final/fork_system_call.cpp
+++ semister-final/fork_system_call.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<unistd.h>
-
+#include<sys/wait.h>
 
 int main() {
 
@@ -11,9 +11,14 @@ int main() {
     child_pid = fork();
 
     if(child_pid == 0) {
-        printf("Child Process\n");
+        // printf("Child Process\n");
+        printf("Child Process PID = %d , Child PID value = %d\n",getpid(), child_pid);
     }else if(child_pid > 0) {
+
+        // wait(NULL); // Wait for the child process to finish
+
         printf("Parent Process\n");
+        
     }else {
         perror("Forkk failed!!\n");
     }
